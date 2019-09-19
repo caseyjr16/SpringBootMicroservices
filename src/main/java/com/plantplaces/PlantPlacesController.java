e9e811c778046a1ca5201c19449c39cf17beec7e
Administrator <Administrator@DESKTOP-099UQRG>
2019-09-19 15:16:38 -0400

    T

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -1,16 +1,33 @@
 package com.plantplaces;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+/**
+//handle the / start endpoint 
+
+ *
+ * @author Administrator
+ * @return hhh
+ */
 @Controller
 public class PlantPlacesController {
 
-	@RequestMapping("/start")
-	public String start() {
+	@RequestMapping(value="/start", method=RequestMethod.GET)
+	public String read() {
 		
 		return "start";
 	}
+	
+@PostMapping("/start")	
+public String create() {
+		
+	
+		return "start";
+	}
+
 	@RequestMapping("/")
 	public String index() {
 		
@@ -18,4 +35,4 @@ public class PlantPlacesController {
 	}
 }
 
-//commit to git
+
